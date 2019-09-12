44afceb2ffb0319f4793a185cc264c8436999a2f
anshtyagi23 <tyagiansh23@gmail.com>
2019-09-12 23:09:31 +0530

    Update ques12.c

--- ques12.c
+++ ques12.c
@@ -1,13 +1,22 @@
 #include<stdio.h>
+#include<math.h>
 //Ansh Tyagi IT 11912079
 int main(){
-	int a,b,c,p,q,r;
+	float a,b,c,p,q,r,dist,len;
 	printf("enter the constt for ax+by+c=0:\n");
 	scanf("%d %d %d",&a,&b,&c);
 	printf("coordinates for the centre of circle:\n");
 	scanf("%d %d",&p,&q);
 	printf("enter the radius:\n");
 	scanf("%d",&r);
+	dist=sqrt((a*p+b*q+c)/((a*a)+(b*b)));
+	if(dist<r){
+		len=sqrt((r*r)-(dist*dist));
+		printf("Area=%f\n",len*dist);
+	}else{
+		printf("no intersection");
+	}
+		
 	
 	return 0;
 }
