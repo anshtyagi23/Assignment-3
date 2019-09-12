d86b78c32c5d52ed6507ddbe2cc64d448c9c043f
anshtyagi23 <tyagiansh23@gmail.com>
2019-09-12 23:04:31 +0530

    Add files via upload

---
+++ ques12.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+//Ansh Tyagi IT 11912079
+int main(){
+	int a,b,c,p,q,r;
+	printf("enter the constt for ax+by+c=0:\n");
+	scanf("%d %d %d",&a,&b,&c);
+	printf("coordinates for the centre of circle:\n");
+	scanf("%d %d",&p,&q);
+	printf("enter the radius:\n");
+	scanf("%d",&r);
+	
+	return 0;
+}
